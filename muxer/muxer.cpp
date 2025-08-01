2e4245b69da19cd470c25f5745d3d22074562351
lijunjie2232 <2533584225@qq.com>
2025-08-01 19:51:31 +0900

    init commit

---
+++ muxer/muxer.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    printf("%s\n", avcodec_configuration())
+}
